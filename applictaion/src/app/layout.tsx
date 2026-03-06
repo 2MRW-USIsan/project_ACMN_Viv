9d043bc280f33630942719711ea34534b837ca67
2MRW-USIsan <161544474+2MRW-USIsan@users.noreply.github.com>
2026-03-06 17:58:25 +0000

    Next.js App Setup

---
+++ applictaion/src/app/layout.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`acmn-application`}>
+        {children}
+      </body>
+    </html>
+  );
+}
